938a5632d8a986a08cb83cae45f37464a148c0cb
Averageasd <nhuynh9497@gmail.com>
2025-10-27 01:12:22 -0400

    init project.

---
+++ src/main/java/org/example/storemanagementbestpractice/StoreManagementBestPracticeApplication.java
@@ -0,0 +1,13 @@
+package org.example.storemanagementbestpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreManagementBestPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreManagementBestPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/storemanagementbestpractice/StoreManagementBestPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.storemanagementbestpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreManagementBestPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
