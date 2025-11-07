d26a611019c990bc24a7cdc4254f583d15cb96bb
Nguyen Huynh <nhuynh9497@gmail.com>
2025-11-06 22:14:50 -0500

    added H2 and implemented data seed.

--- src/main/java/org/example/storemanagementbestpractice/config/ProjectSecurityConfig.java
+++ src/main/java/org/example/storemanagementbestpractice/config/ProjectSecurityConfig.java
@@ -1,17 +1,20 @@
 package org.example.storemanagementbestpractice.config;
 
-import org.example.storemanagementbestpractice.handler.RestAuthFailureHandler;
-import org.example.storemanagementbestpractice.handler.RestAutheSuccessHandler;
+import org.example.storemanagementbestpractice.services.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -20,49 +23,31 @@ public class ProjectSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
                         requests ->
                                 requests
                                         .requestMatchers("/product").authenticated()
                                         .requestMatchers("/category").authenticated()
-                                        .requestMatchers("/login").permitAll()
-                )
-                .formLogin(
-                        form ->
-                                form
-                                        .loginProcessingUrl("/login")
-                                        .permitAll()
-                                        .successHandler(new RestAutheSuccessHandler())
-                                        .failureHandler(new RestAuthFailureHandler())
-                )
-                .logout(
-                        logout ->
-                                logout
-                                        .logoutUrl("/logout")
-                                        .invalidateHttpSession(true)
-                                        .deleteCookies("JSESSIONID")
-                                        .permitAll()
                 )
                 .httpBasic(Customizer.withDefaults());
         return http.build();
     }
 
     @Bean
-    public InMemoryUserDetailsManager userDetailService() {
-        UserDetails admin = User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("12345")
-                .roles("USER")
-                .build();
+    public UserDetailsService userDetailsService() {
+        return new CustomUserDetailsService();
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("admin")
-                .password("54321")
-                .roles("USER", "ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(admin, user);
+    @Bean
+    public AuthenticationManager authenticationManager(UserDetailsService userDetailsService) throws Exception {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService);
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        return new ProviderManager(daoAuthenticationProvider);
     }
 }
 
---
+++ src/main/java/org/example/storemanagementbestpractice/services/CustomUserDetailsService.java
@@ -0,0 +1,23 @@
+package org.example.storemanagementbestpractice.services;
+
+import org.example.storemanagementbestpractice.repository.UserDetailsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserDetailsRepository userDetailsRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userDetailsRepository.findByUsername(username)
+                .orElseThrow(
+                        () -> new UsernameNotFoundException("Username not found")
+                );
+    }
+}
