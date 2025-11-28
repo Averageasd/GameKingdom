463474a57fd742698ee43d852c06cf0c061bc6fd
Nguyen Huynh <nhuynh9497@gmail.com>
2025-11-27 20:57:00 -0500

    set up CORS to allow frontend to communicate.

--- run-docker-compose-dev.ps1
+++ run-docker-compose-dev.ps1
@@ -1,3 +1,4 @@
+docker-compose -f docker-compose.prod.yml -p app-prod down -v
 docker-compose -f docker-compose.dev.yml -p app-dev down -v
 docker-compose -f docker-compose.dev.yml -p app-dev build --no-cache
 docker-compose -f docker-compose.dev.yml -p app-dev up -d
--- run-docker-compose-prod.ps1
+++ run-docker-compose-prod.ps1
@@ -1,3 +1,4 @@
+docker-compose -f docker-compose.prod.yml -p app-dev down -v
 docker-compose -f docker-compose.prod.yml -p app-prod down -v
 docker-compose -f docker-compose.prod.yml -p app-prod build --no-cache
 docker-compose -f docker-compose.prod.yml -p app-prod up -d
